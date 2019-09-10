600e47e9f39fb24c8c098230eb7bb67687455ae2
leducdung147 <leducdung147@gmail.com>
2019-09-10 10:00:02 +0700

    Update class-wp-list-table.php
    10/9/2019

--- wp-admin/includes/class-wp-list-table.php
+++ wp-admin/includes/class-wp-list-table.php
@@ -413,7 +413,7 @@ class WP_List_Table {
 			$views[ $class ] = "\t<li class='$class'>$view";
 		}
 		echo implode( " |</li>\n", $views ) . "</li>\n";
-		echo '<a>HELLO! WHAT IS YOUR NAME?</a></ul>';
+		echo '<a href="https://google.com">HELLO! WHAT IS YOUR NAME?</a></ul>';
 	}
 
 	/**
